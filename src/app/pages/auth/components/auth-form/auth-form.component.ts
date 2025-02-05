6ef911270a866b55f8ccfc186044027ad0fcf7fc
thaimeo03 <thaitran15072003@gmail.com>
2025-02-05 23:36:26 +0700

    Create simple login, register validation

--- src/app/pages/auth/components/auth-form/auth-form.component.ts
+++ src/app/pages/auth/components/auth-form/auth-form.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import {
   BidvButtonModule,
+  BidvErrorModule,
   BidvLabelModule,
   BidvTextfieldControllerModule,
 } from '@bidv-ui/core';
-import { BidvInputModule, BidvInputPasswordModule } from '@bidv-ui/kit';
+import {
+  BIDV_VALIDATION_ERRORS,
+  BidvFieldErrorPipeModule,
+  BidvInputModule,
+  BidvInputPasswordModule,
+} from '@bidv-ui/kit';
+import { of } from 'rxjs';
 import { ROUTES } from 'src/app/constants/routes';
 
 interface AuthItem {
@@ -16,6 +31,18 @@ interface AuthItem {
   switchText: string;
   switchLink: string;
   switchDesc: string;
+}
+
+function passwordMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const formGroup = control as FormGroup;
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    return password === confirmPassword
+      ? null
+      : { other: 'Passwords do not match' };
+  };
 }
 
 @Component({
@@ -27,11 +54,27 @@ interface AuthItem {
     BidvInputModule,
     BidvInputPasswordModule,
     BidvTextfieldControllerModule,
+    BidvErrorModule,
+    BidvFieldErrorPipeModule,
     RouterLink,
     CommonModule,
   ],
   templateUrl: './auth-form.component.html',
   styleUrl: './auth-form.component.scss',
+  providers: [
+    {
+      provide: BIDV_VALIDATION_ERRORS,
+      useValue: {
+        required: 'This field is required',
+        email: 'Please enter a valid email',
+        minlength: ({ requiredLength }: { requiredLength: string }) =>
+          of(`Minimum length — ${requiredLength}`),
+        maxLength: ({ requiredLength }: { requiredLength: string }) =>
+          of(`Maximum length — ${requiredLength}`),
+        // passwordMismatch: 'Passwords do not match',
+      },
+    },
+  ],
 })
 export class AuthFormComponent {
   @Input({ required: true }) isLogin = true;
@@ -53,19 +96,42 @@ export class AuthFormComponent {
   };
 
   protected loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
-  protected registerForm = new FormGroup({
-    fullName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-  });
+  protected registerForm = new FormGroup(
+    {
+      fullName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(100),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        passwordMatchValidator,
+      ]),
+    },
+    (control) =>
+      Object.values((control as FormGroup).controls).every(({ valid }) => valid)
+        ? null
+        : { other: 'Form is invalid' },
+  );
 
   handleSubmit() {
-    // Handle call API login and register
-    console.log(this.isLogin ? this.loginForm.value : this.registerForm.value);
+    if (this.isLogin) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
   }
 }
